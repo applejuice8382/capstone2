6e562d0a02bc76a1555da39db2f9e7b32d8c11f4
applejuice8382 <lemon585@naver.com>
2020-05-26 13:48:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/capstone2/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.capstone2;
+
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//mypage2
+
+//
+//        ImageView imageView = (ImageView) findViewById(R.id.imageProfile);
+//
+//        GradientDrawable drawable=
+//                (GradientDrawable) getDrawable(R.drawable.imageround);
+//
+//        imageView.setBackground(drawable);
+//        imageView.setClipToOutline(true);
+//
+//        ListView listview = findViewById(R.id.listview);
+//
+//        ArrayList<Customer> items = new ArrayList<>();
+//        items.add(new Customer("사진 1", "010-1234-5678", "남자"));
+//        items.add(new Customer("사진 2", "010-4321-8765", "남자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 1", "010-1234-5678", "남자"));
+//        items.add(new Customer("사진 2", "010-4321-8765", "남자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//        items.add(new Customer("사진 3", "010-8765-4321", "여자"));
+//
+//
+//        MyAdapter adapter = new MyAdapter(items, getApplicationContext());
+//
+//        listview.setAdapter(adapter);
+
+
+    }
+
+}
