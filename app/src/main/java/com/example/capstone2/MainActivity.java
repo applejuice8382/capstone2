425507446e28a6ad822acef05571a3526fa0ffda
applejuice8382 <lemon585@naver.com>
2020-05-30 14:45:07 +0900

    mainactivity update

--- app/src/main/java/com/example/capstone2/MainActivity.java
+++ app/src/main/java/com/example/capstone2/MainActivity.java
@@ -60,71 +60,71 @@ public class MainActivity extends AppCompatActivity {
 //
 //        listview.setAdapter(adapter);
 
-        GridView gridView = (GridView) findViewById(R.id.gridView);
-
-        // 어댑터 안에 데이터 담기
-        final GridAdapter adapter = new GridAdapter();
-
-        adapter.addItem(new GridItem("소녀시대"));
-        adapter.addItem(new GridItem("소녀시대2"));
-        adapter.addItem(new GridItem("소녀시대3"));
-        adapter.addItem(new GridItem("소녀시대4"));
-        adapter.addItem(new GridItem("소녀시대5"));
-        adapter.addItem(new GridItem("소녀시대6"));
-
-        // 리스트 뷰에 어댑터 설정
-        gridView.setAdapter(adapter);
-
-        // 이벤트 처리 리스너 설정
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                GridItem item = (GridItem) adapter.getItem(position);
-                Toast.makeText(getApplicationContext(), "선택 :" + item.getName(), Toast.LENGTH_LONG).show();
-            }
-        });
-
-    }
-
-    public class GridAdapter extends BaseAdapter {
-        ArrayList<GridItem> items = new ArrayList<GridItem>();
-
-
-        // Generate > implement methods
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        public void addItem(GridItem item) {
-            items.add(item);
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            GridItemView view = null;
-            if (convertView == null) {
-                view = new GridItemView(getApplicationContext());
-            } else {
-                view = (GridItemView) convertView;
-            }
-
-            GridItem item = items.get(position);
-
-            view.setName(item.getName());
-
-            return view;
-        }
+//        GridView gridView = (GridView) findViewById(R.id.gridView);
+//
+//        // 어댑터 안에 데이터 담기
+//        final GridAdapter adapter = new GridAdapter();
+//
+//        adapter.addItem(new GridItem("소녀시대"));
+//        adapter.addItem(new GridItem("소녀시대2"));
+//        adapter.addItem(new GridItem("소녀시대3"));
+//        adapter.addItem(new GridItem("소녀시대4"));
+//        adapter.addItem(new GridItem("소녀시대5"));
+//        adapter.addItem(new GridItem("소녀시대6"));
+//
+//        // 리스트 뷰에 어댑터 설정
+//        gridView.setAdapter(adapter);
+//
+//        // 이벤트 처리 리스너 설정
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                GridItem item = (GridItem) adapter.getItem(position);
+//                Toast.makeText(getApplicationContext(), "선택 :" + item.getName(), Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//    }
+//
+//    public class GridAdapter extends BaseAdapter {
+//        ArrayList<GridItem> items = new ArrayList<GridItem>();
+//
+//
+//        // Generate > implement methods
+//        @Override
+//        public int getCount() {
+//            return items.size();
+//        }
+//
+//        public void addItem(GridItem item) {
+//            items.add(item);
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return items.get(position);
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            return position;
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            GridItemView view = null;
+//            if (convertView == null) {
+//                view = new GridItemView(getApplicationContext());
+//            } else {
+//                view = (GridItemView) convertView;
+//            }
+//
+//            GridItem item = items.get(position);
+//
+//            view.setName(item.getName());
+//
+//            return view;
+//        }
 
     }
 
