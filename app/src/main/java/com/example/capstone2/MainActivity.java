864f7763ef61c626576476718b076dad016a15d4
applejuice8382 <lemon585@naver.com>
2020-09-27 16:08:55 +0900

    first page

--- app/src/main/java/com/example/capstone2/MainActivity.java
+++ app/src/main/java/com/example/capstone2/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.capstone2;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.drawable.GradientDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,23 +12,50 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    String myJSON;
+
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD = "address";
+
+    JSONArray peoples = null;
+
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        list = (ListView) findViewById(R.id.listview);
+        personList = new ArrayList<HashMap<String, String>>();
+        getData("http://10.20.54.211:8070/PHP_connection.php"); //수정 필요
 
 
 
@@ -93,46 +121,119 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class GridAdapter extends BaseAdapter {
-        ArrayList<GridItem> items = new ArrayList<GridItem>();
+    protected void showList() {
+        try {
+            JSONObject jsonObj;
+            jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
 
+            for (int i = 0; i < peoples.length(); i++) {
+                JSONObject c = peoples.getJSONObject(i);
+                String name = c.getString(TAG_NAME);
+                String address = c.getString(TAG_ADD);
 
-        // Generate > implement methods
-        @Override
-        public int getCount() {
-            return items.size();
-        }
+                HashMap<String, String> persons = new HashMap<String, String>();
 
-        public void addItem(GridItem item) {
-            items.add(item);
-        }
+                persons.put(TAG_NAME, name);
+                persons.put(TAG_ADD, address);
 
-        @Override
-        public Object getItem(int position) {
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            GridItemView view = null;
-            if (convertView == null) {
-                view = new GridItemView(getApplicationContext());
-            } else {
-                view = (GridItemView) convertView;
+                personList.add(persons);
             }
 
-            GridItem item = items.get(position);
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, personList, R.layout.listview_custom,
+                    new String[]{TAG_NAME, TAG_ADD},
+                    new int[]{R.id.name, R.id.address}
+            );
 
-            view.setName(item.getName());
+            list.setAdapter(adapter);
 
-            return view;
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
     }
+
+    public void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
+
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+
+//    public class GridAdapter extends BaseAdapter {
+//        ArrayList<GridItem> items = new ArrayList<GridItem>();
+//
+//
+//        // Generate > implement methods
+//        @Override
+//        public int getCount() {
+//            return items.size();
+//        }
+//
+//        public void addItem(GridItem item) {
+//            items.add(item);
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return items.get(position);
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            return position;
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            GridItemView view = null;
+//            if (convertView == null) {
+//                view = new GridItemView(getApplicationContext());
+//            } else {
+//                view = (GridItemView) convertView;
+//            }
+//
+//            GridItem item = items.get(position);
+//
+//            view.setName(item.getName());
+//
+//            return view;
+//        }
+//
+//    }
 
 }
--- app/src/main/java/com/example/capstone2/MyAdapter.java
+++ app/src/main/java/com/example/capstone2/MyAdapter.java
@@ -1,57 +1,58 @@
-package com.example.capstone2;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
+//package com.example.capstone2;
+//
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import java.util.ArrayList;
+//
+//public class MyAdapter extends BaseAdapter {
+//
+//   ArrayList<Customer> items;
+//    Context context;
+//
+//    public MyAdapter(ArrayList<Customer> items, Context context){
+//        this.items = items;
+//        this.context = context;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return items.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int i) {
+//        return items.get(i);
+//    }
+//
+//    @Override
+//    public long getItemId(int i) {
+//        return i;
+//    }
 
-public class MyAdapter extends BaseAdapter {
+//    @Override
+//    public View getView(int i, View view, ViewGroup viewGroup) {
+//
+//        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        view = inflater.inflate(R.layout.listview_custom, viewGroup, false);
+//
+//        TextView tv_name = view.findViewById(R.id.textview_name);
+//       tv_name.setText(items.get(i).name);
+//
+//       TextView tv_gender = view.findViewById(R.id.textview_gender);
+//        tv_gender.setText(items.get(i).gender);
+//
+//        TextView tv_phone = view.findViewById(R.id.textview_phone);
+//        tv_phone.setText(items.get(i).phone);
+//
+//        return view;
+//    }
 
-    ArrayList<Customer> items;
-    Context context;
-
-    public MyAdapter(ArrayList<Customer> items, Context context){
-        this.items = items;
-        this.context = context;
-    }
-
-    @Override
-    public int getCount() {
-        return items.size();
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return items.get(i);
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view = inflater.inflate(R.layout.listview_custom, viewGroup, false);
-
-        TextView tv_name = view.findViewById(R.id.textview_name);
-        tv_name.setText(items.get(i).name);
-
-        TextView tv_gender = view.findViewById(R.id.textview_gender);
-        tv_gender.setText(items.get(i).gender);
-
-        TextView tv_phone = view.findViewById(R.id.textview_phone);
-        tv_phone.setText(items.get(i).phone);
-
-        return view;
-    }
-
-}
-
+//}
